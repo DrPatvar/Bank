ad39a933b542865113af1ac2e8a711a720fb6bbd
DrPatvar <Dr.Patvar@yandex.ru>
2023-10-04 09:31:58 +0300

    swagger

--- src/main/java/com/example/bank/BankApplication.java
+++ src/main/java/com/example/bank/BankApplication.java
@@ -1,9 +1,22 @@
 package com.example.bank;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "REST API documentation Bank account",
+                version = "1.0.0",
+                description = """
+                        Приложение для создания Банковских счетов и перевода денег между ними.  
+                        """,
+                contact = @Contact(name = "Shurkov Ivan", email = "shurkoff@mail.ru")
+        )
+)
 public class BankApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/bank/appConfig/AppConfig.java
+++ src/main/java/com/example/bank/appConfig/AppConfig.java
@@ -1,8 +1,8 @@
 package com.example.bank.appConfig;
 
+import org.h2.tools.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.h2.tools.Server;
 
 import java.sql.SQLException;
 
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class AppConfig {
 
     @Bean(initMethod = "start", destroyMethod = "stop")
-    Server h2Server() throws SQLException{
+    Server h2Server() throws SQLException {
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 
